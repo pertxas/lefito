3f06a0662b6db792274cb50f3c5a83d315293dd6
pertxas <pertxas@middleearthcoin.com>
2014-11-30 22:58:46 +0100

    payloads lists

--- lefito.py
+++ lefito.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .api import Parameters, connect_tor, testip, menuppal
+from api import Parameters, connect_tor, testip, menuppal
 import argparse
 
 # --------------------------------------------------------------------------
